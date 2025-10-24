99aab83550af535bbf38e7a30c39ef8bb6370c0c
이재문 <990914s@gmail.com>
2025-10-24 21:06:17 +0900

    [level 2] Title: 올바른 괄호, Time: 0.37 ms, Memory: 3.84 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,24 @@
+#include <string>
+
+using namespace std;
+
+bool solution(string s)
+{
+	 int balance = 0;
+	 for (auto c : s) 
+	 {
+		if (c == '(')	
+		{
+			++balance;
+		} 
+		else if (c == ')') 
+		{
+			--balance;
+			if (balance < 0) 
+			{
+				return false;
+			}
+		}
+	 }
+	return balance == 0;
+}
